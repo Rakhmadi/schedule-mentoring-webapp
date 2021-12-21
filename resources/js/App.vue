a901fd65927b1fcc7828a2fb59ed66dbc5385922
rakhmadi <rakhmadiwalker@gmail.com>
2021-12-21 15:47:49 +0700

    commit add tailwind

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,7 +1,32 @@
 <template>
-    <div class="flex justify-center mt-24">
-        <h1 class="text-2xl font-bold text-gray-700">Vue 3 App</h1>
+    <div>
+            <div class="flex justify-between h-14 bg-blue-100 px-4">
+                <div class="flex items-center ">
+                    <button class="menu transition ease-in-out delay-75 rounded-full bg-blue-200 hover:bg-blue-300 text-blue-600 font-medium w-max h-max py-1 px-3 m-1 ">
+                        <span class="p-0 m-0">Course</span>
+                    </button>
+                    <button class="menu transition ease-in-out delay-75 rounded-full bg-blue-200 hover:bg-blue-300 text-blue-600 font-medium w-max h-max py-1 px-3 m-1 ">
+                        <span class="p-0 m-0">Mentor</span>
+                    </button>
+                    <button class="menu transition ease-in-out delay-75 rounded-full bg-blue-200 hover:bg-blue-300 text-blue-600 font-medium w-max h-max py-1 px-3 m-1 ">
+                        <span class="p-0 m-0">Schedule</span>
+                    </button>
+                </div>
+                <div class="flex items-center">
+                    <button class="menu transition ease-in-out delay-75 rounded-full bg-blue-200 hover:bg-blue-300 text-blue-600 font-medium w-max h-max py-1 px-3 m-1 flex flex-row items-center">
+                        <i class="mr-2">
+                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M17 16L21 12M21 12L17 8M21 12H7M13 16V17C13 17.7956 12.6839 18.5587 12.1213 19.1213C11.5587 19.6839 10.7956 20 10 20H6C5.20435 20 4.44129 19.6839 3.87868 19.1213C3.31607 18.5587 3 17.7956 3 17V7C3 6.20435 3.31607 5.44129 3.87868 4.87868C4.44129 4.31607 5.20435 4 6 4H10C10.7956 4 11.5587 4.31607 12.1213 4.87868C12.6839 5.44129 13 6.20435 13 7V8" stroke="#3F3F46" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </i>
+                        <span>Logout</span>
+                    </button>
+                </div>
+            </div>
     </div>
+    <main>
+        Wollcwom
+    </main>
 </template>
 
 <script>
